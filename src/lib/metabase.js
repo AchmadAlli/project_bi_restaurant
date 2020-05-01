31b48c91e2a13e6f14eede5eab383e0189efd469
Achmad Ali B <achmadali@comeapp.id>
2020-05-01 16:50:55 +0700

    remove temp component

--- src/lib/metabase.js
+++ src/lib/metabase.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const getIframeUrlFromKey = (questionId) => {
-  const METABASE_SITE_URL = 'http://restaurant-dashboard-kel-6.herokuapp.com';
+  const METABASE_SITE_URL = 'https://restaurant-dashboard-kel-6.herokuapp.com';
   const METABASE_SECRET_KEY = 'b8a62360e18b21f1a20f00b2d5b2cfb97098af2598b7851f460afeb9b1d8c7d0';
 
   const payload = {
